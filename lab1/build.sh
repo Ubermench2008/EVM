7b081549228463148302dc670dd7c0add0532f66
Ubermench2008 <ivan@Bruh>
2024-09-08 22:55:40 +0700

    Перемещен скрипт build.sh в папку lab1

--- lab1/build.sh
+++ lab1/build.sh
@@ -2,11 +2,11 @@
 
 mkdir -p build
 
-gcc -o build/output main.c other_file.c
+gcc -o build/output main.c -lm
 
 if [ $? -eq 0 ]; then
     echo "Компиляция завершена успешно."
 else
     echo "Ошибка компиляции."
     exit 1
-
+fi
